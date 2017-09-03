e6ed0171cd427a458cd552e98e09ad8a9dbc2c80
Róger Valderrama <roger.andres18@gmail.com>
2017-09-03 10:35:05 -0600

    Initial UI

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,13 +9,12 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1200, 700));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
@@ -27,6 +26,7 @@ public class Main extends Application {
 // todo switch scenes
 // todo alertbox
 // todo usar lambdas "->"
+// todo snackbar for finish saving
 
 
 
