862d7cc38ceb826d2b80b1993e49f07131083b36
Róger Valderrama <roger.andres18@gmail.com>
2017-09-06 00:50:18 -0600

    Second Scene Login in Start

--- src/LinkedDB/StartController.java
+++ src/LinkedDB/StartController.java
@@ -4,14 +4,11 @@ import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable{
+public class StartController implements Initializable{
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
-    public static void errordialog(){
-        System.out.println("Error");
-    }
-
-
 }
+
+
---
+++ src/LinkedDB/Store.java
@@ -0,0 +1,106 @@
+package LinkedDB;
+
+import com.google.gson.Gson;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+public class Store {
+
+    private String atributo;
+    private String value;
+    private String nombre;
+    private String defecto;
+    private String requiered;
+    private String StoreName;
+
+    final String currentdirectory = System.getProperty("user.dir");
+
+    Gson gson = new Gson();
+
+    Path path = Paths.get(currentdirectory);
+
+    Boolean X = Files.exists(path);
+
+    public Store(String atributo, String value, String nombre, String defecto, String required){
+        this.atributo = atributo;
+        this.value = value;
+        this.nombre = nombre;
+        this.defecto = defecto;
+        this.requiered = required;
+    }
+
+    public String getCurrentdirectory() {
+        return currentdirectory;
+    }
+
+    public String getAtributo() {
+        return atributo;
+    }
+
+    public void setAtributo(String atributo) {
+        this.atributo = atributo;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDefecto() {
+        return defecto;
+    }
+
+    public void setDefecto(String defecto) {
+        this.defecto = defecto;
+    }
+
+    public String getRequiered() {
+        return requiered;
+    }
+
+    public void setRequiered(String requiered) {
+        this.requiered = requiered;
+    }
+
+
+    private String Validar(){ //todo REVISAR RETURN
+        try{
+            switch(getAtributo()){
+                case "int": Integer.parseInt(getValue());
+                break;
+
+                case "float": Float.parseFloat(getValue());
+                break;
+                //case "fecha" PREGUNTAR
+                default: Controller.errordialog();
+
+            }
+
+        }catch(Exception e){
+            System.out.println("ERROR EN VALIDACION");
+        }
+        return getValue();
+    }
+
+//    public static Store stringtostore(String value){
+//        //String = nombre:valor:atributo:defecto:requiered
+//        value.split()
+//    }
+
+//    public static String storetostring(Store store){
+//        return
+//    }
+}
